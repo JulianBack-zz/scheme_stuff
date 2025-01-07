05aa8fd8507bac5ec23ce25267148d940055b287
Julian Back <julian.back@gmail.com>
2025-01-07 11:08:23 +0000

    Improve lexer tests

--- lexer.scm
+++ lexer.scm
@@ -1,13 +1,13 @@
 (import (chicken port))
 
 ;;; Read a token from input and return it
-(define (read-token)
+(define (get-token)
   (let loop ((c (peek-char)))
     (cond
      ((eof-object? c) c)
      ((char-whitespace? c) (read-char) (loop (peek-char)))
-     ((char-alphabetic? c) (read-identifier))
-     ((char-numeric? c) (read-number))
+     ((char-alphabetic? c) (get-identifier))
+     ((char-numeric? c) (get-number))
      ((char=? c #\;) (read-char) '(SEMICOLON))
      ((char=? c #\+) (read-char) '(PLUS))
      ((char=? c #\-) (read-char) '(MINUS))
@@ -20,7 +20,7 @@
           '(COLON)))
      (else (read-char) (cons 'ERROR c)))))
 
-(define (read-identifier)
+(define (get-identifier)
   (let loop ((c (peek-char)) (identifier '()))
     (cond
      ((or (char-alphabetic? c) (char-numeric? c))
@@ -29,7 +29,7 @@
      (else
       (list 'IDENTIFIER (list->string (reverse identifier)))))))
 
-(define (read-number)
+(define (get-number)
   (let loop ((c (peek-char)) (number '()))
     (cond
      ((char-numeric? c)
@@ -38,13 +38,23 @@
      (else
       (list 'NUMBER (string->number (list->string (reverse number))))))))
 
-;; Example usage:
-(define (test)
-  (let ((t (read-token)))
-    (if (not (eof-object? t))
+(define (get-tokens)
+  (let loop ((t (get-token)) (tokens '()))
+    (if (eof-object? t)
+        (reverse tokens)
+        (loop (get-token) (cons t tokens)))))
+  
+(define (test-get-tokens input expected-result)
+  (let ((result (with-input-from-string input get-tokens)))
+    (if (equal? result expected-result)
         (begin
-          (print t)
-          (test)))))
+          (write-string input)
+          (write-line " OK"))
+        (begin
+          (write-string input)
+          (write-line " FAIL")
+          (write result)))))
 
-(with-input-from-string "var x:integer; x := 42+ alpha ;" test)
-
+(test-get-tokens "var x:integer; x := 42+ alpha ;"
+                 '((IDENTIFIER "var") (IDENTIFIER "x") (COLON) (IDENTIFIER "integer") (SEMICOLON)
+                   (IDENTIFIER "x") (ASSIGN) (NUMBER 42) (PLUS) (IDENTIFIER "alpha") (SEMICOLON)))
