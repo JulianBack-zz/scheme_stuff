4808b03e61240417288865687b4b7a98f9a348e5
Julian Back <julian.back@gmail.com>
2025-01-26 13:17:40 +0000

    Add more tokens and comments

--- lexer.scm
+++ lexer.scm
@@ -5,8 +5,8 @@
 ;;; 1. DONE Support line numbering
 ;;; 2. DONE Use hash table for reserved words
 ;;; 3. DONE Store filename in lexer record
-;;; 4. Add additional tokens
-;;; 5. Comments, including nested
+;;; 4. DONE Add additional tokens
+;;; 5. DONE Comments, including nested
 ;;; 6. Strings and characters including hex and escapes
 ;;; 7. Hex integers
 ;;; 8. Floating point constants
@@ -38,7 +38,24 @@
                        ("var" VAR)
                        ("begin" BEGIN)
                        ("end" END)
-                       ("function" FUNCTION))))
+                       ("procedure" PROCEDURE)
+                       ("div" DIV)
+                       ("mod" MOD)
+                       ("or" OR)
+                       ("if" IF)
+                       ("then" THEN)
+                       ("else" ELSE)
+                       ("elseif" ELSEIF)
+                       ("while" WHILE)
+                       ("do" DO)
+                       ("repeat" REPEAT)
+                       ("until" UNTIL)
+                       ("array" ARRAY)
+                       ("of" OF)
+                       ("record" RECORD)
+                       ("const" CONST)
+                       ("type" TYPE)
+                       ("module" MODULE))))
 
 ;;; Read a token from port and return it
 (define (get-token-from-port lexer)
@@ -59,13 +76,31 @@
            ((char=? c #\/) (read-char port) '(DIV))
            ((char=? c #\*) (read-char port) '(MUL))
            ((char=? c #\=) (read-char port) '(EQUAL))
-           ((char=? c #\() (read-char port) '(OPEN))
            ((char=? c #\)) (read-char port) '(CLOSE))
            ((char=? c #\,) (read-char port) '(COMMA))
+           ((char=? c #\&) (read-char port) '(AND))
+           ((char=? c #\#) (read-char port) '(NE))
+           ((char=? c #\~) (read-char port) '(NOT))
+           ((char=? c #\() (read-char port)
+            (if (char=? #\* (peek-char port))
+                (begin
+                  (read-char port)
+                  (if (eof-object? (skip-comment lexer))
+                      (cons 'ERROR "EOF in comment")
+                      (loop (peek-char port))))
+                '(OPEN)))
            ((char=? c #\:) (read-char port)
             (if (char=? #\= (peek-char port))
                 (begin (read-char port) '(ASSIGN))
                 '(COLON)))
+           ((char=? c #\>) (read-char port)
+            (if (char=? #\= (peek-char port))
+                (begin (read-char port) '(GE))
+                '(GT)))
+           ((char=? c #\<) (read-char port)
+            (if (char=? #\= (peek-char port))
+                (begin (read-char port) '(LE))
+                '(LT)))
            (else (read-char port) (cons 'ERROR c)))))))
 
 (define (get-identifier port)
@@ -89,6 +124,28 @@
       (loop (peek-char port) (cons c number)))
      (else
       (list 'INT (string->number (list->string (reverse number))))))))
+
+; Returns EOF object if end of file hit
+(define (skip-comment lexer)
+  (let ((port (lexer-get-port lexer)))
+    (let loop ((c (peek-char port)))
+      (cond
+       ((eof-object? c) c)
+       ((char=? c #\newline)
+        (inc-line-number lexer)
+	    (loop (read-char port)))
+       ((char=? c #\()
+        (if (char=? #\* (peek-char port))
+            (begin
+              (read-char port)
+              (skip-comment lexer)
+              (loop (peek-char port)))))
+       ((char=? c #\*)
+        (if (char=? #\) (peek-char port))
+            (read-char port)
+            (loop (peek-char port))))
+       (else
+        (loop (read-char port)))))))
 
 (define *token-trace* #f)
 
@@ -129,8 +186,8 @@
           (newline)))))
 
 (define (test-lexer)
-  (test-get-tokens "function test(alpha, beta);\nvar x:integer;\nbegin\n x := 42+ alpha ;\nend;\n"
-                   '((FUNCTION 1) (ID "test" 1) (OPEN 1) (ID "alpha" 1) (COMMA 1) (ID "beta" 1) (CLOSE 1) (SEMICOLON 1)
+  (test-get-tokens "procedure test(alpha, beta); (* test function *)\nvar x:integer;\nbegin\n x := 42+ alpha ; (* A (* nested *) comment *)\nend;\n"
+                   '((PROCEDURE 1) (ID "test" 1) (OPEN 1) (ID "alpha" 1) (COMMA 1) (ID "beta" 1) (CLOSE 1) (SEMICOLON 1)
                      (VAR 2) (ID "x" 2) (COLON 2) (INTEGER 2) (SEMICOLON 2)
                      (BEGIN 3)
                      (ID "x" 4) (ASSIGN 4) (INT 42 4) (PLUS 4) (ID "alpha" 4) (SEMICOLON 4)
